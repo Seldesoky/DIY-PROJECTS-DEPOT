cf149f6898eadaa713d13132d51004445d3b63c8
seldesoky <sameh.s.eldesoky@gmail.com>
2024-10-06 21:08:14 -0400

    frontendbasic

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the DIY Project Hub</h1>
+      <p>Explore and share DIY projects with the community.</p>
+    </div>
+  );
+};
+
+export default Home;
